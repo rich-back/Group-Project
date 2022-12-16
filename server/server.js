1b3249d299caaf7281eb28f04a7922968b488ae3
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-12-16 16:34:24 +0000

    server setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json())
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true }, (err, client) => {
+  if(err){
+    console.log(err);
+  }
+
+  const db = client.db('elements_table');
+  const elementsCollection = db.collection('elements');
+  const elementsRouter = createRouter(elementsCollection)
+  app.use('/api/elements', elementsRouter);
+
+  app.listen(9000, function(){
+    console.log(`app listening on port ${this.address().port}`);
+  })
+})
