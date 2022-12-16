c3f952ee5d788eeec95a5067fa2cd21626070f18
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-12-16 15:59:51 +0000

    Welcome Page

---
+++ client/src/welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Welcome = () => (
+  <div>
+    <h4>Welcome</h4>
+    <p>Welcome to Element_Able - Your guide to the Period Table</p>
+  </div>
+);
+
+export default Home;
