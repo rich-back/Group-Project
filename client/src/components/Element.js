a06e92ed62340f778bc41103faeac1d3c2b5cdd7
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-17 14:40:19 +0000

    Put more info in the element component

--- client/src/components/Element.js
+++ client/src/components/Element.js
@@ -6,9 +6,14 @@ const Element = ({ element }) => {
 
     return (
         <div className="element">
-            <Link to={`/periodictable/element/${element._id}`}><h4>{element.atomicNumber}.{element.name}</h4></Link>
-            <p><b> Standard State: </b>{element.standardState}</p>
-            <p><code>{ JSON.stringify(element) }</code></p>
+            <Link to={`/periodictable/element/${element._id}`}><h4>{element.atomicNumber}. {element.name}</h4></Link>
+            <ul className="element-properties">
+                <li><b>Standard State:</b><span>{element.standardState || "unknown"}</span></li>
+                <li><b>Atomic Mass:</b><span>{element.atomicMass || "unknown"}</span></li>
+                <li><b>Electron Configuration:</b><span>{element.electronicConfiguration || "unknown"}</span></li>
+                <li><b>Group block:</b><span>{element.groupBlock}</span></li>
+                <li><b>Year discovered:</b><span>{element.yearDiscovered}</span></li>
+            </ul>
         </div>
     )
 }
