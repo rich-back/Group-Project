da6a5055274d7b25e62a9597553fd4d121dfaeb6
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-19 14:26:49 +0000

    Some simple tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+
+  test('renders app name somewhere', () => {
+      render(<App />);
+      const appName = screen.getByText(/element_able/i);
+      expect(appName).toBeInTheDocument();
+  });
+
+  test('has link to welcome', () => {
+      render(<App />);
+      const welcomeLink = screen.getByText(/Welcome Page/i, { selector: 'a' });
+      expect(welcomeLink).toBeInTheDocument();
+  });
+
+  test('has link to periodic table', () => {
+      render(<App />);
+      const welcomeLink = screen.getByText(/Periodic Table/i, { selector: 'a' });
+      expect(welcomeLink).toBeInTheDocument();
+      expect(welcomeLink).toHaveAttribute('href', expect.stringContaining('periodictable'));
+  });
+
+  test('has link to quiz', () => {
+      render(<App />);
+      const welcomeLink = screen.getByText(/Quiz/i, { selector: 'a' });
+      expect(welcomeLink).toBeInTheDocument();
+      expect(welcomeLink).toHaveAttribute('href', expect.stringContaining('quiz'));
+  });
 });
