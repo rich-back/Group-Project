2408afa561eef831f89d1b8bb1d8b38bb71e47c1
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-19 15:29:31 +0000

    Display highscores as a table
    Also demonstrate using a font

--- client/src/components/HighscoresComponent.js
+++ client/src/components/HighscoresComponent.js
@@ -3,6 +3,19 @@ import { useEffect, useState } from 'react';
 import { getTop10ByMonth } from '../services/HighscoresService.js';
 import { formatDateAsMonth } from '../utilities/format_date.js';
 
+const formatOrdinal = (number) => {
+    switch (number) {
+        case 1:
+            return '1st';
+        case 2:
+            return '2nd';
+        case 3:
+            return '3rd';
+        default:
+            return `${number}th`;
+    }
+};
+
 const HighscoresComponent = ({month}) => {
     const currentMonth = formatDateAsMonth(new Date());
     if (!month)
@@ -15,17 +28,27 @@ const HighscoresComponent = ({month}) => {
         .then(data => setHighscores(data));
     }, [month])
 
-    const highscoreItems = highscores.map(highscore => {
-        return <li>
-            <span className="score">{highscore.score}</span>
-            <span className="name">{highscore.name}</span>
-        </li>
+    const highscoreItems = highscores.map((highscore, index) => {
+        return <tr>
+            <td className="place">{formatOrdinal(index+1)}</td>
+            <td className="score">{highscore.score}</td>
+            <td className="name">{highscore.name}</td>
+        </tr>
     });
 
     return (
-        <ol className="highscores">
-            { highscoreItems }
-        </ol>
+        <table className="highscores">
+            <thead>
+                <tr>
+                    <th>Position</th>
+                    <th>Score</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                { highscoreItems }
+            </tbody>
+        </table>
     );
 };
 
