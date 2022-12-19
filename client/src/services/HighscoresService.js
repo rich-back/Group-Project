2d1ae0e5b7d7ea34ed7eb53080c3742953bcd428
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-19 15:00:32 +0000

    postHighscore function

--- client/src/services/HighscoresService.js
+++ client/src/services/HighscoresService.js
@@ -4,3 +4,12 @@ export const getTop10ByMonth = (month) => {
     return fetch(`${baseURL}/top10/${month}`)
         .then(res => res.json())
 }
+
+export const postHighscore = (payload) => {
+    return fetch(baseURL, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(res => res.json())
+}
