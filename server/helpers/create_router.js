26108f3e705f1084a260149ce93fb32149ceee0f
Benjamin Smith <bsmith@cabbage.org.uk>
2022-12-19 14:07:17 +0000

    Add highscores api, including /api/highscores/top10/:month

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
+const handleError = (res, err) => {
+  console.error(err);
+  res.status(500);
+  res.json({ status: 500, error: err });
+};
+
 const createRouter = function(collection) {
 
   const router = express.Router();
@@ -28,6 +34,8 @@ const createRouter = function(collection) {
       res.json({ status: 500, error: err });
     });
   });
+
+  /* get an item based on it's name attribute */
   router.get('/name/:name', (req, res) => {
     const name = req.params.name;
     collection
@@ -38,6 +46,25 @@ const createRouter = function(collection) {
       res.status(500);
       res.json({ status: 500, error: err });
     });
+  });
+
+  /* get the top 10 items by score for a given month */
+  /* the month is in the format 2022-12 */
+  router.get('/top10/:month', (req, res) => {
+    const month = req.params.month;
+    if (!month.match(/^\d{4}-\d{2}$/)) {
+      return handleError(res, "month in wrong format");
+    }
+
+    const date = { $regex: new RegExp(`^${month}`) };
+    collection
+    .find(
+      { date },
+      { sort: { 'score': -1 }, limit: 10 }
+    )
+    .toArray()
+    .then((docs) => res.json(docs))
+    .catch(err => handleError(res, err));
   });
 
   router.post('/', (req, res) => {
