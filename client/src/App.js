f923335b17cc5492400b9c5711ec2f08a885267a
Cordula Gunst <cordulagunst@Cordulas-MacBook-Air.local>
2022-12-19 13:56:20 +0000

    routes ammended for two defferent quiz

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import PTable from "./pages/PTable";
 import { getElements } from "./services/ElementsService";
 import Quiz from "./pages/Quiz";
 import SElementPage from "./pages/SElementPage";
+import QuizComponent from "./components/QuizComponent";
+import Trivia from "./components/Trivia";
 
 const App = () => {
 
@@ -24,6 +26,8 @@ const App = () => {
         <Route path="/" element={<Welcome />} />
         <Route path="/periodictable" element ={<PTable allElements={allElements} />} />
         <Route path="/quiz" element ={<Quiz allElements={allElements} />} />
+        <Route path="/quiz/state" element ={<QuizComponent allElements={allElements} />} />
+        <Route path="/quiz/trivia" element ={<Trivia allElements={allElements} />} />
         {/* <Route path="/periodictable/element/:id" element ={< SElemtentPage allElements={allElements}/>} /> */}
         <Route path="/element/:name" element ={< SElementPage allElements={allElements}/>} />
 
