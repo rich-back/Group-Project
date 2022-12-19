eebc48e688bcd47f422b8029d871132e119d39f1
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-12-19 16:02:13 +0000

    updated answer handling to give user the correct answer before moving to next question

--- client/src/components/StateQuiz.js
+++ client/src/components/StateQuiz.js
@@ -1,10 +1,10 @@
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 
 
 const QuizComponent = ({ allElements }) => {
   const [randomItem, updateRandomItem] = useState ("Let's Begin")
   const [score, updateScore]= useState (0)
-  const [answer, updateAnswer] = useState ("The Answer Was : ")
+  const [answer, updateAnswer] = useState (null)
 
   function returnRandomItem() {
     const randomindex = Math.floor(Math.random() * allElements.length);
@@ -22,14 +22,12 @@ const QuizComponent = ({ allElements }) => {
   const handleAnswer = ((value)=>{
     if (value.target.value === randomItem.standardState){
       addAPoint()
-      returnRandomItem()
-      updateAnswer(`CORRECT : The Answer Was ${randomItem.standardState}`)
+      updateAnswer(`CORRECT : ${randomItem.name} is a ${randomItem.standardState} `)
     }
-
       else {
-        returnRandomItem()
-        updateAnswer(`WRONG : The Answer Was ${randomItem.standardState}`)
+        updateAnswer(`WRONG : ${randomItem.name} is a ${randomItem.standardState} `)
       }
+
     })
   return (
     <>
@@ -41,9 +39,12 @@ const QuizComponent = ({ allElements }) => {
       <button value={"solid"} onClick={handleAnswer}>Solid</button>
       <button value={"gas"} onClick={handleAnswer}>Gas</button>
       <button value={"liquid"} onClick={handleAnswer}>Liquid</button>
+      <button value={"state unknown"} onClick={handleAnswer}>State Unknown</button>
 
       <h4>Your Score : {score}</h4>     
       <p>{answer}</p>
+      <button onClick={returnRandomItem}> Next Question </button> 
+
 
 
     </>
