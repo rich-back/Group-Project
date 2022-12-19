7ccb235ce9f34010e89b4862fea696c0b754d0a8
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-12-19 15:52:33 +0000

    added react NavLink functionality to navbar links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
-    return (
-        <ul id="nav">
-            <li>
-            <Link to="/">Welcome Page</Link>
-            </li>
-            <li>
-            <Link to="/periodictable">Periodic Table</Link>
-            </li>
-            <li>
-            <Link to="/quiz">Quiz</Link>
-            </li>
-        </ul>
-    );
-}
+  let activeStyle = {
+    color: "green",
+  };
+
+  return (
+    <>
+      <ul id="nav">
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Welcome Page
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/periodictable"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Periodic Table
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/quiz"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Quiz
+          </NavLink>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default NavBar;
