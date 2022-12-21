c3da1e153388b3afc976e2c7b678b00a2f2fea44
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-12-21 19:09:56 +0000

    css tweaks, no need to merge if folk would prefer not to

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -2,20 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import QuizComponent from "../components/StateQuiz";
 import HighscoresComponent from "../components/HighscoresComponent";
+import icon from "../static/elementablesIcon.svg";
+
 
 const QuizContainer = ({ allElements }) => (
 
   <main className="main-content">
-    <article className = "quiz-home">
+    <article className="quiz-home">
       <h2>Test Your Knowlegdge on the Periodic Table </h2>
-      <Link to={`/quiz/state`}>Take the atomic state test</Link> <br></br>
+      <img src={icon} height="50px" />
+      <Link to={`/quiz/state`}>Take the atomic state test</Link>
+      <br></br>
+      <img src={icon} height="50px" />
       <Link to={`/quiz/trivia`}>Take the Trivia test</Link>
+      <img src={icon} height="50px" />
+
     </article>
     <aside className="boxed-aside">
       <h3>This month's high scores</h3>
       <HighscoresComponent game="state" />
     </aside>
-    </main>
-    );
+  </main>
+);
 
-    export default QuizContainer;
+export default QuizContainer;
