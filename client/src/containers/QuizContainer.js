94be1d6110e5fdcb32faedf98cf8c0e80a11b65e
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-12-21 13:20:28 +0000

    formatting main start buttons in quiz

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -4,6 +4,7 @@ import QuizComponent from "../components/StateQuiz";
 
 const QuizContainer = ({allElements}) => (
     <>
+    <h2>Test Your Knowlegdge on the Periodic Table </h2>
         <Link to={`/quiz/state`}>Take the atomic state test</Link> <br></br>
         <Link to={`/quiz/trivia`}>Take the Trivia test</Link>
     </>
